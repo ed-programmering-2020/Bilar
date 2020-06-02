287f9ba1f34d94787d00d52acade0a0f37b788e8
Joel Fredriksson <0117frjo@jonkoping.se>
2020-06-02 13:03:36 +0200

    Har ändrat lite

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -30,12 +30,15 @@
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.HämtaData = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Change = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(112, 63);
+            this.listBox1.Location = new System.Drawing.Point(41, 43);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(183, 212);
             this.listBox1.TabIndex = 0;
@@ -50,16 +53,49 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // HämtaData
+            // 
+            this.HämtaData.Location = new System.Drawing.Point(87, 261);
+            this.HämtaData.Name = "HämtaData";
+            this.HämtaData.Size = new System.Drawing.Size(75, 23);
+            this.HämtaData.TabIndex = 3;
+            this.HämtaData.Text = "Hämta";
+            this.HämtaData.UseVisualStyleBackColor = true;
+            this.HämtaData.Click += new System.EventHandler(this.HämtaData_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(259, 43);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(232, 212);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Change
+            // 
+            this.Change.Location = new System.Drawing.Point(335, 261);
+            this.Change.Name = "Change";
+            this.Change.Size = new System.Drawing.Size(75, 23);
+            this.Change.TabIndex = 5;
+            this.Change.Text = "Ändra";
+            this.Change.UseVisualStyleBackColor = true;
+            this.Change.Click += new System.EventHandler(this.Change_Click);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(658, 340);
+            this.Controls.Add(this.Change);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.HämtaData);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
             this.Name = "Form2";
             this.Text = "Form2";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,5 +103,8 @@
 
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button HämtaData;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Change;
     }
 }
--- Form2.cs
+++ Form2.cs
@@ -17,6 +17,7 @@ namespace BilHyr
 
         string SeleBil = null;
         List<string> BilProperties = new List<string>();
+        string[] BilValues = new string[13];
         string connectionstring = "SERVER=Localhost;DATABASE=bilhyr;" +
             "UID=KurtKurtsson;PASSWORD=ljuset;";
 
@@ -53,6 +54,64 @@ namespace BilHyr
         {
             Form2 server = new Form2();
             server.Close();
+        }
+
+        private void HämtaData_Click(object sender, EventArgs e)
+        {
+            MySqlConnection connection;
+
+            string connectionstring = "SERVER=Localhost;DATABASE=bilhyr;" +
+               "UID=KurtKurtsson;PASSWORD=ljuset;";
+
+
+
+
+            textBox1.Clear();
+            BilProperties.Clear();
+            SeleBil = listBox1.GetItemText(listBox1.SelectedItem);
+            connection = new MySqlConnection(connectionstring);
+            connection.Open();
+            MySqlDataReader dataReader = null;
+            string sqlsats = "SELECT * FROM kunder WHERE Namn = " + "\"" + SeleBil + "\"";
+            MySqlCommand cmd = new MySqlCommand(sqlsats, connection);
+            dataReader = cmd.ExecuteReader();
+            dataReader.Read();
+            for (int i = 0; i < dataReader.FieldCount; i++)
+            {
+                BilProperties.Add(dataReader.GetName(i));
+            }
+            foreach (string str in BilProperties)
+            {
+                textBox1.AppendText(Environment.NewLine + dataReader[str].ToString());
+            }
+
+            connection.Close();
+        }
+
+        private void Change_Click(object sender, EventArgs e)
+        {
+            for (int i = 2; i < BilValues.Length; i++)
+            {
+                BilValues[i] = textBox1.Lines[i];
+            }
+            string valuesDisplay = string.Join(Environment.NewLine, BilValues);
+            MessageBox.Show("Företagets värden har justerats till " + valuesDisplay);
+            connection = new MySqlConnection(connectionstring);
+            connection.Open();
+            MySqlDataReader dataReader = null;
+            string sqlsats = "UPDATE kunder SET Namn = \"" + BilValues[3] + "\", Nummer? = \"" +
+                BilValues[4] + "\", Telefon = \"" + BilValues[5] + "\", Address = \"" +
+                BilValues[6] + "\", Stad = \"" + BilValues[7] + "\", Region = \"" +
+                BilValues[8] + "\", Land = \"" +  "\" WHERE Namn = \"" + SeleBil + "\"";
+            textBox1.Text = sqlsats;
+            MySqlCommand cmd = new MySqlCommand(sqlsats, connection);
+            dataReader = cmd.ExecuteReader();
+            connection.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
